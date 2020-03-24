8d1604ec6a10a92df29b0604e0f05e0b42833387
Vanni Durantini <vannivd@yahoo.com>
2020-03-24 09:57:15 +0100

    Aggiunto metodo per stampare stringa

--- GitExample/src/base/Example1.java
+++ GitExample/src/base/Example1.java
@@ -3,6 +3,10 @@ package base;
 public class Example1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println(metodo1());
+	}
+	
+	public static String metodo1() {
+		return "Hello world!";
 	}
 }
