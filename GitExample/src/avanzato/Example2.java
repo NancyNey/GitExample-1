634a16fe7ba8564ab35e95cb728941f5634a100d
Vanni Durantini <vannivd@yahoo.com>
2020-03-24 10:06:10 +0100

    Aggiunta classe esterna da istanziare con metodo che stampa 2 stringhe concatenate

---
+++ GitExample/src/avanzato/Example2.java
@@ -0,0 +1,9 @@
+package avanzato;
+
+public class Example2 {
+	
+	public String metodo1(String a, String b) {
+		return (a + " " + b);
+	}
+
+}
--- GitExample/src/base/Example1.java
+++ GitExample/src/base/Example1.java
@@ -1,9 +1,13 @@
 package base;
 
+import avanzato.Example2;
+
 public class Example1 {
 	
 	public static void main(String[] args) {
 		System.out.println(metodo1());
+		Example2 e2 = new Example2();
+		System.out.println(e2.metodo1("Ciao da", "Vanni!"));
 	}
 	
 	public static String metodo1() {
